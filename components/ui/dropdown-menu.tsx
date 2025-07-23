4dd7d9132e74082891817bd54ced84c457f7dc33
Chioma Osanebi <chiomaosanebi5@gmail.com>
2025-07-23 22:14:33 +0100

    Fix: resolved ESLint errors and type issues

--- components/ui/dropdown-menu.tsx
+++ components/ui/dropdown-menu.tsx
@@ -2,10 +2,16 @@
 
 import * as DropdownMenuPrimitive from "@radix-ui/react-dropdown-menu";
 import { cn } from "@/lib/utils";
+import type { ComponentPropsWithoutRef, HTMLAttributes } from "react";
+
 
 export const DropdownMenu = DropdownMenuPrimitive.Root;
 export const DropdownMenuTrigger = DropdownMenuPrimitive.Trigger;
-export const DropdownMenuContent = ({ className, ...props }: any) => (
+
+export const DropdownMenuContent = ({
+  className,
+  ...props
+}: ComponentPropsWithoutRef<typeof DropdownMenuPrimitive.Content>) => (
   <DropdownMenuPrimitive.Content
     className={cn(
       "z-50 min-w-[10rem] rounded-md border bg-white p-1 shadow-md",
@@ -14,10 +20,11 @@ export const DropdownMenuContent = ({ className, ...props }: any) => (
     {...props}
   />
 );
+
 export const DropdownMenuItem = ({
   className,
   ...props
-}: React.ComponentPropsWithoutRef<"div">) => (
+}: HTMLAttributes<HTMLDivElement>) => (
   <div
     className={cn(
       "flex cursor-pointer items-center px-3 py-2 text-sm hover:bg-gray-100",
